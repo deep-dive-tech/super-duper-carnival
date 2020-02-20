d97c73616957c25d31022711dc7a0bde067d3470
tristan-c-dixon <56423458+tristan-c-dixon@users.noreply.github.com>
2020-02-20 14:31:04 -0500

    Add files via upload

--- dive_to_csv.py
+++ dive_to_csv.py
@@ -3,9 +3,11 @@ import os
 from datetime import timedelta
 import fitparse
 
+
 # the header fields appearing in the output csv file.  These fields are renamed versions of what appears in the
 # fit field list.
-header_fields = ['timestamp', 'type', 'accel_x', 'accel_y', 'accel_z', 'heart_rate', 'lat', 'lon']
+header_fields = ['timestamp', 'type', 'accel_x', 'accel_y', 'accel_z', 'heart_rate', 'lat', 'lon', 'vel_x', 'vel_y',
+                 'vel_z', 'dis_x', 'dis_y', 'dis_z', 'mag_x', 'mag_y', 'mag_z']
 
 
 def convert_dir_fit_to_csv(input_dir, output_dir):
@@ -34,29 +36,71 @@ def convert_fit_to_csv(in_file, out_file):
     """
     fit_parse = fitparse.FitFile(in_file, data_processor=fitparse.StandardUnitsDataProcessor())
 
+    save_vx = 0
+    save_dx = 0
+    save_vy = 0
+    save_dy = 0
+    save_vz = 0
+    save_dz = 0
+
     data = []
     for m in fit_parse.messages:
         if not hasattr(m, 'fields'):
             continue
 
         # for debug - print out all the fields in the message - can use this to look at the all available information
-        # print(m.fields)
+        print(m.fields)
 
         # turn m.fields array into a fields dictionary for ease of processing (lookup by name)
         fields = {k.name: k.value for k in m.fields}
         if 'compressed_calibrated_accel_x' in fields:
             timestamp = fields['timestamp']
             num_samples = len(fields['compressed_calibrated_accel_x'])
+            vx = [0.0] * (num_samples + 1)
+            dx = [0.0] * (num_samples + 1)
+            vy = [0.0] * (num_samples + 1)
+            dy = [0.0] * (num_samples + 1)
+            vz = [0.0] * (num_samples + 1)
+            dz = [0.0] * (num_samples + 1)
+
+            vx[0] = save_vx
+            dx[0] = save_dx
+            vy[0] = save_vy
+            dy[0] = save_dy
+            vz[0] = save_vz
+            dz[0] = save_dz
+            dt = 1/25
+
             for i in range(num_samples):
+                vx[i+1] = fields['compressed_calibrated_accel_x'][i] * (9.80665/1000) * dt + vx[i]
+                dx[i+1] = vx[i] * dt + dx[i]
+                vy[i + 1] = fields['compressed_calibrated_accel_y'][i] * (9.80665 / 1000) * dt + vy[i]
+                dy[i + 1] = vy[i] * dt + dy[i]
+                vz[i + 1] = fields['compressed_calibrated_accel_z'][i] * (9.80665 / 1000) * dt + vz[i]
+                dz[i + 1] = vz[i] * dt + dz[i]
+
                 # turn this row into the multiple rows dimensioned by number of samples.  The timestamp is adjusted
                 # to offset by the number of samples taken within the message
                 row = {'type': 'A',
-                       'timestamp': timestamp + timedelta(milliseconds=(i * 1000 / num_samples)),
+                       'timestamp': timestamp - timedelta(milliseconds=((num_samples-i-1) * 1000 / num_samples)),
                        'accel_x': fields['compressed_calibrated_accel_x'][i],
                        'accel_y': fields['compressed_calibrated_accel_y'][i],
                        'accel_z': fields['compressed_calibrated_accel_z'][i],
+                       'vel_x': vx[i+1],
+                       'dis_x': dx[i+1],
+                       'vel_y': vy[i + 1],
+                       'dis_y': dy[i + 1],
+                       'vel_z': vz[i + 1],
+                       'dis_z': dz[i + 1],
                        }
                 data.append(row)
+            save_vx = vx[num_samples]
+            save_dx = dx[num_samples]
+            save_vy = vy[num_samples]
+            save_dy = dy[num_samples]
+            save_vz = vz[num_samples]
+            save_dz = dz[num_samples]
+
         elif 'heart_rate' in fields:
             row = {'type': 'H',
                    'timestamp': fields['timestamp'],
@@ -67,6 +111,14 @@ def convert_fit_to_csv(in_file, out_file):
                    'timestamp': fields['timestamp'],
                    'lat': fields['position_lat'],
                    'lon': fields['position_long']}
+            data.append(row)
+
+        if 'mag_x' in fields and fields['mag_x'] is not None:
+            row = {'type': 'M',
+                   'timestamp':fields['timestamp'],
+                   'mag_x': fields['mag_x'],
+                   'mag_y': fields['mag_y'],
+                   'mag_z': fields['mag_z']}
             data.append(row)
 
     # write out csv
